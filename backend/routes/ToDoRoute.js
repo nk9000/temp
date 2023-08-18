a147e1abba6866efecd8b84058ab5351cd3aea5b
Nilesh Kumar <seemadevi1121339@gmail.com>
2023-08-18 19:28:06 +0530

    first commit

---
+++ backend/routes/ToDoRoute.js
@@ -0,0 +1,9 @@
+const {Router} = require("express");
+const { getTodo, saveTodo, updateTodo, deleteTodo } = require("../controllers/ToDoController");
+const router = Router();
+
+router.get('/' , getTodo)
+router.post('/save' , saveTodo)
+router.post('/update' , updateTodo)
+router.post('/delete' , deleteTodo)
+module.exports = router;
